54d09597b393e32d9f807edede88fbed78db1123
alankila <alankila@75748be7-0387-4326-94c7-f41ce0883d2c>
2009-04-03 15:52:49 +0000

    Kill old junk.
    git-svn-id: svn://svn.code.sf.net/p/sidplay-residfp/code/trunk@42 75748be7-0387-4326-94c7-f41ce0883d2c

--- scripts/filter-recompile.sh
+++ scripts/filter-recompile.sh
@@ -5,16 +5,6 @@ set -e
 MYDIR=$(dirname $0)
 cd "$MYDIR/../sidplay-libs-2.1.1"
 
-if [[ -e builders/Makefile ]]; then
-    cd builders && make clean && cd ..
-fi
-
-# ensure psiddrv is not recompiled with xa, I don't have it
-touch libsidplay/src/psiddrv.bin
-
-# workaround a bug in Debian's packaging scripts
-rm -rf debian/libsidplay-dev
-
 # compile
 DEB_BUILD_OPTIONS="debug nostrip" MAKEFLAGS=-j3 fakeroot debian/rules binary-arch
 
