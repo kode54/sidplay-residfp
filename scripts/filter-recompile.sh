890ad33ff08a7f46a7221928a27f558d4541b294
alankila <alankila@75748be7-0387-4326-94c7-f41ce0883d2c>
2009-06-21 06:30:10 +0000

    Use CXX flags from environment.
    git-svn-id: svn://svn.code.sf.net/p/sidplay-residfp/code/trunk@127 75748be7-0387-4326-94c7-f41ce0883d2c

--- scripts/filter-recompile.sh
+++ scripts/filter-recompile.sh
@@ -6,7 +6,10 @@ MYDIR=$(dirname $0)
 cd "$MYDIR/../sidplay-libs-2.1.1"
 
 # compile
-DEB_BUILD_OPTIONS="debug nostrip" MAKEFLAGS=-j3 fakeroot debian/rules binary-arch
+CXXFLAGS="-O2 -ffast-math" \
+DEB_BUILD_OPTIONS="debug nostrip" \
+MAKEFLAGS=-j3 \
+fakeroot debian/rules binary-arch
 
 # install
 cd ..
